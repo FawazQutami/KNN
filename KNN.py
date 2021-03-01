5ea77f9e677ff50ea50e86c6bc4862c275a7bd1e
apriLLion <28001843+FawazQutami@users.noreply.github.com>
2021-03-01 23:21:39 +0200

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,7 +1,6 @@
 # File: KNN.py
 
 
-
 import numpy as np
 from collections import Counter
 
@@ -10,15 +9,20 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+import warnings
+
+warnings.filterwarnings("ignore")
 
 """
     There are various methods for calculating the distance
     between the points, of which the most commonly known
     methods are –
-        1. Euclidian,
+        1. Euclidean,
         2. Manhattan (for continuous) and
         3. Hamming distance (for categorical).
-    """
+"""
+
+
 def euclidean_distances(point1, point2):
     """ Euclidean Distance """
     """
@@ -31,33 +35,33 @@ def euclidean_distances(point1, point2):
 
 
 class KNN:
+    """ K Nearest Neighbor Algorithm """
     """
-       KNN: K Nearest Neighbor Algorithm.
-           Knn is simple to implement and mostly used for classification.
-           Knn is easy to interpret.
-           Knn for large datasets requires a lot of memory and gets slow
-               to predict because of distance calculations
-           Knn accuracy can be broken down if there are many predictors
-           Knn doesn't generate insights
+       Knn is simple to implement and mostly used for classification.
+       Knn is easy to interpret.
+       Knn for large datasets requires a lot of memory and gets slow
+            to predict because of distance calculations
+        Knn accuracy can be broken down if there are many predictors
+        Knn doesn't generate insights
     """
 
     def __init__(self, k=3):
+        """ KNN Constructor """
         """
-        Constructor
         K represents the number of the nearest neighbors
         that we used to classify new data points.
         """
         self.k = k
 
-    def fit(self, _X_train, _y_train):
+    def fit(self, x_trn, y_trn):
         """ Fitting Data """
-        self.X_train = _X_train
-        self.y_train = _y_train
+        self.X_train = x_trn
+        self.y_train = y_trn
 
-    def predict(self, _X_test):
+    def predict(self, x_tst):
         """ Predict the classes of the test data """
         predicted_labels = [self.predict_labels(x)
-                            for x in _X_test]
+                            for x in x_tst]
         return np.array(predicted_labels)
 
     def predict_labels(self, point1):
@@ -65,7 +69,7 @@ class KNN:
         # Calculate the Euclidean distance
         distances_List = [euclidean_distances(point1, point2)
                           for point2 in self.X_train]
-        
+
         """
         Once the distance of a new observation from the points in
         our training set has been measured, the next step is to
@@ -73,21 +77,21 @@ class KNN:
         The number of points to be considered is defined by the
         value of k.
         """
-        # Return K indexs of the smallest distances
+        # Return K index of the smallest distances
         k_index = np.argsort(distances_List)[:self.k]
-        
-        ## VOTING for the outcome:
+
+        # VOTING for the outcome:
         # Find the associated labels
-        k_index_labels = [self.y_train[i] for i in k_index]    
+        k_index_labels = [self.y_train[i] for i in k_index]
         # Get the most common class - the final outcome
         most_common_label = Counter(k_index_labels).most_common(1)
-        
+
         return most_common_label[0][0]
 
 
-def accuracy(_y_test, _prediction):
-    """ Calculate the Accurecy """
-    acc = np.sum(_y_test == _prediction) / len(_y_test)
+def accuracy(y_tst, y_predicted):
+    """ Calculate the Accuracy """
+    acc = np.sum(y_tst == y_predicted) / len(y_tst)
     return acc
 
 
@@ -103,16 +107,17 @@ def draw_plot(xs, ys):
 
 
 if __name__ == '__main__':
+    # Load Iris data from sklearn datasets
     iris = datasets.load_iris()
+    # Create X and y data
     X, y = iris.data, iris.target
 
     draw_plot(X, y)
-    
+
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size=0.2,
                                                         random_state=1234)
 
-    
     K = 3
     """
     Choosing the right value of K is called parameter tuning and 
@@ -121,10 +126,11 @@ if __name__ == '__main__':
         - Odd value of K is always selected to avoid confusion between 2 
         classes.
     """
+    # Create KNN classifier
     clf = KNN(k=K)
+    # Fit x and y training data
     clf.fit(X_train, y_train)
-    predictions = clf.predict(X_test)
-
-    print("KNN Accuracy is: ", accuracy(y_test, predictions))
-
-    
+    # Predict the labels of the y testing data
+    y_predictions = clf.predict(X_test)
+    # What is the accuracy rate of the model
+    print("KNN Accuracy is: ", accuracy(y_test, y_predictions))
